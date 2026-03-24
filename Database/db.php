01062b0cd0349cc8e2027fa8623a456c1bb50660
alexioamana-afk <alexioamana@gmail.com>
2026-03-24 08:18:34 +0100

    second commit

--- Database/db.php
+++ Database/db.php
@@ -2,7 +2,7 @@
 
 $host = "localhost";
 $user = "root";
-$password = "";
+$password = "password";
 $database = "eshop";
 
 $connection = mysqli_connect($host,$user,$password,$database);
@@ -10,6 +10,6 @@ $connection = mysqli_connect($host,$user,$password,$database);
 if(!$connection){
     echo "Connection failed: ";
 }else {
-    echo " connected successfully"
+    echo " connected successfully";
 }
 
